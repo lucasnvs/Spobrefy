b4631e9d3631d93a5bfc56040c322279cfe254f3
lucasnvs <lucasli.nev@hotmail.com>
2023-05-09 02:03:28 -0300

    Criando class Music e adicionando get extra

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,17 +1,14 @@
+import com.spobrefy.Music;
 import com.spobrefy.users.*;
 
 public class Sistema {
     public static void main(String[] args) {
-            User usuario = new User("Carlos" ,"carlos@c.c", "sder845");
-            Admin admin = new Admin("adm1", "Sam", "sam@s.com" , "sam123pastel", "05501692023" , "10/01/2004", 19);
-            Artist artist = new Artist("Sam", "sam@s.com" , "sam123pastel", "05501692023" , "10/01/2004", 19);
-            usuario.imprimir();
-            System.out.println();
-            admin.imprimir();
-            System.out.println();
-            artist.imprimir();
-            System.out.println("teste");
-        }
+        Artist hariel = new Artist("Mc Hariel", "haridade@email.com", "23ha2023", "023233453432", "20/04/2000", 23);
+        Music musica1 = new Music("Pecado", hariel, 3.5);
+        
+        System.out.println(musica1.getAuthor().getNickname());
+        System.out.println(musica1.getRating());
+    }
 }
 
    
