17b6b5d205689d9ff321dd19b8f929e0ea064cdd
lucasnvs <lucasli.nev@hotmail.com>
2023-05-14 01:51:11 -0300

    Adicionando métodos na class Music com erro no método create.

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import com.spobrefy.content.Music;
 import com.spobrefy.users.*;
 
 public class Sistema {
     public static void main(String[] args) {
-            Admin admin = Admin.create();
-            admin.print();
+
+        List<Artist> artistList = new ArrayList<>();
+
+        Artist teto = new Artist("Teto", "teti@gmail.com", "30praUm", "2335345345", "10/10/2000", 22);
+        Artist matue = new Artist("Matuê", "mat@gmail.com", "30praDois", "23389634545", "09/11/1999", 23);
+        artistList.add(teto);
+        artistList.add(matue);
+        
+
+        Music PayPal = Music.create(artistList);
+        Music conexoes = Music.create(artistList);
+        
+        PayPal.print();
+        conexoes.print();
     }
 }
--- src/com/spobrefy/content/Music.java
+++ src/com/spobrefy/content/Music.java
@@ -1,4 +1,8 @@
 package com.spobrefy.content;
+
+import java.util.List;
+import java.util.Scanner;
+
 import com.spobrefy.users.Artist;
 
 public class Music {
@@ -8,13 +12,13 @@ public class Music {
     private Artist author;
     private Double rating;
 
-    public Music(String name, Artist author, Double rating){
+    // rating é adicionado depois de acordo com aceitação do publico
+    public Music(String name, Artist author) {
         this.name = name;
         this.author = author;
-        this.rating = rating;
         idMusic = ++count;
     }
-    
+
     public int getId() {
         return idMusic;
     }
@@ -29,6 +33,40 @@ public class Music {
 
     public Double getRating() {
         return rating;
+    }
+
+    private static Artist findAuthor(List<Artist> list, String authorName) {
+        for (Artist artist : list) {
+            if (artist.getNickname().equals(authorName)) {
+                return artist;
+            }
+        }
+        return null;
+    }
+
+    public static Music create(List<Artist> list) {
+        // nao consegui consertar o bug
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual o título?");
+        String title = scanner.nextLine();
+        if (!scanner.hasNextLine()) {
+            scanner.close();
+            return null;
+        }
+        System.out.println("Qual o nome do autor?");
+        String authorName = scanner.nextLine();
+
+
+        Artist author = findAuthor(list, authorName);
+        Music music = new Music(title, author);
+        scanner.close();
+        return music;
+    }
+
+    public void print() {
+        String text = String.format("Id: %d\nTítulo: %s\nAutor: %s\nRating: %2.1f", idMusic, name, author.getEmail(),
+                rating);
+        System.out.println(text);
     }
 
 }
