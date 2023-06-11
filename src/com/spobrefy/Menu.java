ebec111bc8aeaec5e951879ad00877f865303301
lucasnvs <lucasli.nev@hotmail.com>
2023-06-11 01:00:04 -0300

    separando fun: Menu e Sistema

--- src/com/spobrefy/Menu.java
+++ src/com/spobrefy/Menu.java
@@ -1,65 +1,32 @@
 package com.spobrefy;
 import java.util.Scanner;
 
-import com.spobrefy.users.User;
 
 public class Menu {
-    // talvez mover as funções para o sistema e usar aq pelo parametro de sistema, deixando apenas os dialogs;
+    public Scanner scan = new Scanner(System.in);
+
     public void init(Sistema sistema) {
         System.out.println("=======================================================================");
-        System.out.println("Olá querido usuário, para acessar o sistema será necessário logar :(");
-        if(!login(sistema)) return;
+        System.out.println("Olá querido usuário, para acessar o sistema será necessário logar:");
+        if(!dialogLogin(sistema)) return;
         // entra no sistema e faz as ações;
     }
 
-    private void registerUser(Sistema sistema) {
-        User user = User.create(sistema.scan);
-        sistema.addUser(user);
-        System.out.println("=======================================================================");
-        System.out.println("CONTA REGISTRADA COM SUCESSO!");
-        System.out.println("=======================================================================");
-    }
-
-    private Boolean login(Sistema sistema) {
-        
+    private Boolean dialogLogin(Sistema sistema) {
         Integer aux;
-
-        Scanner sc = sistema.scan;
-
-        System.out.println("=======================================================================");
-        System.out.println("Login de Usuário:");
+        Scanner sc = this.scan;
         System.out.println("| 1 - Logar\n| 2 - Não possuo conta");
         aux = sc.nextInt();
         sc.nextLine();
 
         if(aux == 1) {
-            System.out.println("=======================================================================");
-            System.out.println("+ Qual o seu nickname?");
-            String nick = sc.nextLine();
-            System.out.println("+ Qual sua senha?");
-            String pass = sc.nextLine();
-
-            if(!sistema.loginVerify(nick, pass)) {
-                System.out.println("=======================================================================");
-                System.out.println("SENHA OU NICKNAME INCORRETOS!");
-                System.out.println("=======================================================================");
-                System.out.println("| Deseja tentar novamente?\n| 1 - Tentar novamente\n| 2 - Desistir...");
-                System.out.println("=======================================================================");
-                aux = sc.nextInt();
-
-                if(aux == 1) login(sistema);
-                return false;
-            };
-            System.out.println("=======================================================================");      
-            System.out.println(String.format("LOGADO COM SUCESSO! SEJA BEM VINDO DE VOLTA %s", nick));
-            System.out.println("=======================================================================");
-            return true;
+            if(sistema.login()) return true;
         }
 
         if(aux == 2) {
-            registerUser(sistema);
-            login(sistema);
-        };
+            sistema.registerUser();
+            if(sistema.login()) return true;   
+        }
 
         return false;
     }
--- src/com/spobrefy/Sistema.java
+++ src/com/spobrefy/Sistema.java
@@ -2,7 +2,6 @@ package com.spobrefy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import com.spobrefy.content.Music;
 import com.spobrefy.content.Playlist;
 import com.spobrefy.users.*;
@@ -15,11 +14,19 @@ public class Sistema {
         this.allUsers = new ArrayList<>();
     }
 
-    public void addUser(User u) {
+    private void addUser(User u) {
         this.allUsers.add(u);
     }
 
-    public boolean loginVerify(String nick, String pass) {
+    public void registerUser() {
+        User user = User.create(this.scan);
+        this.addUser(user);
+        System.out.println("=======================================================================");
+        System.out.println("CONTA REGISTRADA COM SUCESSO!");
+        System.out.println("=======================================================================");
+    }
+
+    private boolean loginVerify(String nick, String pass) {
         for (User user : allUsers) {
             if (user.getNickname().equals(nick) && user.getPassword().equals(pass)) {
                 return true;
@@ -27,42 +34,74 @@ public class Sistema {
         }
         return false;
     }
+
+    public Boolean login() {
+
+            Integer aux;
+            System.out.println("LOGIN DE USUÁRIO:");
+            System.out.println("=======================================================================");
+            System.out.println("+ Qual o seu nickname?");
+            String nick = this.scan.nextLine();
+            System.out.println("+ Qual sua senha?");
+            String pass = this.scan.nextLine();
+
+            if(!loginVerify(nick, pass)) {
+                System.out.println("=======================================================================");
+                System.out.println("SENHA OU NICKNAME INCORRETOS!");
+                System.out.println("=======================================================================");
+                System.out.println("| Deseja tentar novamente?\n| 1 - Tentar novamente\n| 2 - Desistir...");
+                System.out.println("=======================================================================");
+                aux = this.scan.nextInt();
+
+                if(aux == 1) {
+                    this.scan.nextLine();
+                    login();
+                }
+                return false;
+            }
+            
+            System.out.println("=======================================================================");      
+            System.out.println(String.format("LOGADO COM SUCESSO! SEJA BEM VINDO DE VOLTA %s", nick));
+            System.out.println("=======================================================================");
+            return true;
+    }
+
     public static void main(String[] args) {
         // testes
         Sistema sistema = new Sistema();
 
-        Scanner scanner = sistema.scan;
-        List<Artist> artistList = new ArrayList<>();
+        // Scanner scanner = sistema.scan;
+        // List<Artist> artistList = new ArrayList<>();
 
-        User lukinhas222 = new User("lukinhas222", "l@l.com", "pastel2020");
-        Artist teto = new Artist("Teto", "teti@gmail.com", "30praUm", "2335345345", "10/10/2000", 22);
-        Artist matue = new Artist("Matue", "mat@gmail.com", "30praDois", "23389634545", "09/11/1999", 23);
-        artistList.add(teto);
-        artistList.add(matue);
+        // User lukinhas222 = new User("lukinhas222", "l@l.com", "pastel2020");
+        // Artist teto = new Artist("Teto", "teti@gmail.com", "30praUm", "2335345345", "10/10/2000", 22);
+        // Artist matue = new Artist("Matue", "mat@gmail.com", "30praDois", "23389634545", "09/11/1999", 23);
+        // artistList.add(teto);
+        // artistList.add(matue);
         
-        sistema.addUser(teto);
-        sistema.addUser(matue);
-        sistema.addUser(lukinhas222);
+        // sistema.addUser(teto);
+        // sistema.addUser(matue);
+        // sistema.addUser(lukinhas222);
 
-        // Menu menu = new Menu();
-        // menu.init(sistema);
+        Menu menu = new Menu();
+        menu.init(sistema);
 
-        Music PayPal = new Music("PayPal", teto);
-        Music conexoes = new Music("Conexões de Máfia", matue);
+        // Music PayPal = new Music("PayPal", teto);
+        // Music conexoes = new Music("Conexões de Máfia", matue);
 
-        ArrayList<Music> musicas = new ArrayList<>();
-        musicas.add(PayPal);
-        musicas.add(conexoes);
+        // ArrayList<Music> musicas = new ArrayList<>();
+        // musicas.add(PayPal);
+        // musicas.add(conexoes);
                 
-        PayPal.print();
-        conexoes.print();
+        // PayPal.print();
+        // conexoes.print();
 
-        Playlist trintaPraUm = new Playlist(matue, "Melhores da 30praUm", musicas);
+        // Playlist trintaPraUm = new Playlist(matue, "Melhores da 30praUm", musicas);
 
-        lukinhas222.addPlaylist(trintaPraUm);
-        trintaPraUm.print();
-        lukinhas222.print();
+        // lukinhas222.addPlaylist(trintaPraUm);
+        // trintaPraUm.print();
+        // lukinhas222.print();
         
-        scanner.close();
+        // scanner.close();
     }
 }
