b9de3ba04bf2c37ea27b3ce7637f338f1461c160
lucasnvs <lucasli.nev@hotmail.com>
2023-06-14 21:52:49 -0300

    Separando Testes

---
+++ src/com/spobrefy/test/allTest.java
@@ -0,0 +1,43 @@
+package com.spobrefy.test;
+
+import com.spobrefy.Sistema;
+
+public class allTest {
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+
+        // Scanner scanner = sistema.scan;
+        // List<Artist> artistList = new ArrayList<>();
+
+        // User lukinhas222 = new User("lukinhas222", "l@l.com", "pastel2020");
+        // Artist teto = new Artist("Teto", "teti@gmail.com", "30praUm", "2335345345", "10/10/2000", 22);
+        // Artist matue = new Artist("Matue", "mat@gmail.com", "30praDois", "23389634545", "09/11/1999", 23);
+        // artistList.add(teto);
+        // artistList.add(matue);
+        
+        // sistema.addUser(teto);
+        // sistema.addUser(matue);
+        // sistema.addUser(lukinhas222);
+
+        // Menu menu = new Menu();
+        // menu.init(sistema);
+        
+        // Music PayPal = new Music("PayPal", teto);
+        // Music conexoes = new Music("Conexões de Máfia", matue);
+
+        // ArrayList<Music> musicas = new ArrayList<>();
+        // musicas.add(PayPal);
+        // musicas.add(conexoes);
+                
+        // PayPal.print();
+        // conexoes.print();
+
+        // Playlist trintaPraUm = new Playlist(matue, "Melhores da 30praUm", musicas);
+
+        // lukinhas222.addPlaylist(trintaPraUm);
+        // trintaPraUm.print();
+        // lukinhas222.print();
+        
+        // scanner.close();
+    }
+}
